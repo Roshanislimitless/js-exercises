0027e4104b0ac7ddc3cce3e08310735d36e0a679
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-04-03 13:21:18 +0200

    Done with I-floats, Math.round

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var totalnumber = numberOfStudents + numberOfMentors;
+var percentageOfStudents = Math.round((numberOfStudents/totalnumber)*100);
+var percentageOfMentors = Math.round((numberOfMentors/totalnumber)*100);
+
+console.log(totalnumber);
+console.log(percentageOfStudents);
+console.log(percentageOfMentors);
+console.log("Percentage of students: " + percentageOfStudents + "%");
+console.log("Percentage of mentors: " + percentageOfMentors + "%");
+
