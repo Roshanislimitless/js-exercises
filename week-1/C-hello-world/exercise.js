2088491d28f5afba444c773039b5bb47a1b8f2d9
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-04-03 10:34:50 +0200

    done 1

--- week-1/C-hello-world/exercise.js
+++ week-1/C-hello-world/exercise.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+console.log("Hello world from roshan");
+console.log("hello world agin from roshan sapkota");
+console.log(123);
+console.log(true);
+console.log(false);
+
