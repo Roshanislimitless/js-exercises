dbe56c5c03401e78a2a27d1f056a6dee2de99eea
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-04-04 14:58:09 +0200

    done with week 1 other level 1

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -6,7 +6,7 @@ var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? it randomly multiplies 10 with a random decimal number between 0 and 1
+// - How many parameters does the function take? 0-1  0 included and 1 excluded 
+// - What is the function name? function name is getNumber 
+// - Where is the function called? with what parameters? function is called on result with no parameter.
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,14 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+    return w1 + w2;
 }
 
 var result = s('code', 'your');
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. it concat the parameterss 1 and w2 
+// - How many parameters does the function take? 2
+// - What is the function name? s
+// - Where is the function called? with what parameters? called on result 
+// - What does the function return? codeyour
+// - How can this function be improved? by doing w1 + w2 and naming the function with a word.
