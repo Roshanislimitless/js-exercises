c3d32d35da2b83826d1d28afe5c24e132f452742
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-04-06 14:20:51 +0200

    donw with level 2

--- week-1-practise/level-2/3-currency-conversion.js
+++ week-1-practise/level-2/3-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(price) {
+  var currencyformat = price.toFixed(2);
+  return "$" + currencyformat*1.4;
+}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
--- week-1-practise/level-2/4-currency-conversion-2.js
+++ week-1-practise/level-2/4-currency-conversion-2.js
@@ -7,8 +7,13 @@
   Find a way to add 1% to all currency conversions (think about the DRY principle)
 */
 
-function convertToUSD(price) {}
-function convertToBRL(price) {}
+function convertToUSD(price) {
+   return "$" + price * 1.4;
+}
+function convertToBRL(price) {
+   return "R$" + ((price * 5.78) + (price * 5.78) * (1 / 100));
+
+}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
