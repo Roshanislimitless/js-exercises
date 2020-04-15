25d2cbc63a9a1243880f926803cadec6f578e7c6
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-04-15 06:38:45 +0200

    revision

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,19 +1,23 @@
 
 
-
-
-function greetings(mentor){
-    var upperCase = mentor.toUpperCase();
-    return upperCase;
-    
+function calcPercentageStu(mentors, students){
+    var total = mentors + students;
+    var PercentageStudents = Math.round((students/total)*100);
+    return PercentageStudents
+}
+function calcPercentageMen(mentors, students){
+    var total = mentors + students;
+    var PercentageMentors = Math.round((mentors/total)*100);
+    return PercentageMentors;
 }
 
-console.log(greetings("Daniel"));
+function finalMessage(mentors, students){
+    return "Percentage student: " + calcPercentageStu(mentors,students) + "\n" + "Percentage mentors: " + calcPercentageMen(mentors, students);
+}
 
-console.log(greetings("inina"));
 
-console.log(greetings("mimi"));
 
-console.log(greetings("rob"));
 
-console.log(greetings("yohannes"));
+console.log(calcPercentageStu(15,8));
+console.log(calcPercentageMen(15,8));
+console.log(finalMessage(15,8));
