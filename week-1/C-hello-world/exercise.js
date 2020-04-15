fb0d4c456cffb7077626c29f2c73b0cb0ff79a98
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-04-15 05:08:06 +0200

    just a revision

--- week-1/C-hello-world/exercise.js
+++ week-1/C-hello-world/exercise.js
@@ -1,7 +1,3 @@
-console.log("Hello world");
-console.log("Hello world from roshan");
-console.log("hello world agin from roshan sapkota");
-console.log(123);
-console.log(true);
-console.log(false);
+let name = "Ilya";
 
+console.log(3);
